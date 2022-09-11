81318d365e19da898c935dc781b8a71caa11b1bc
magalercio <magalercio.gui@gmail.com>
2022-09-11 20:42:41 -0300

    Arquivos principais
    Criado os arquivos mains e o diretório de kvs.
    Transformando main.kv num gerenciador de telas.
    Criado ajustespage e homepage

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
 
+
+class HomePage(Screen):
+    pass
+
+
+class AjustesPage(Screen):
+    pass
+
+
+GUI = Builder.load_file("main.kv") #tem que ficar depois de todas as classes de paginas, para que elas sejam criadas
+# Antes de cliar o APP.
+class MainApp(App):
+
+    def build(self):
+        return GUI
+
+
+MainApp().run()
