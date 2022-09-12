810cfe1f6076cf622444561c8434a892bdaed61e
magalercio <magalercio.gui@gmail.com>
2022-09-12 13:36:51 -0300

    Funções primárias, organizacao do projeto
    Criando Layout das paginas existentes.
    Criando função de navegação entre telas.
    Alterando cor de fundo e inserindo logo principal.
    Arrumando layout das paginas com floatlayout.
    Criação dos botoes personalizados do menu main. Através da criação de classes label_button e image_button.
    Desmembrando main.py em => Main.py, botoes.py, telas.py

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen
-
-
-class HomePage(Screen):
-    pass
-
-
-class AjustesPage(Screen):
-    pass
-
+from telas import *
+from botoes import *
 
 GUI = Builder.load_file("main.kv") #tem que ficar depois de todas as classes de paginas, para que elas sejam criadas
-# Antes de cliar o APP.
+# Antes de criar o APP.
+
+
 class MainApp(App):
 
     def build(self):
         return GUI
+
+    def mudar_tela(self, id_tela):
+        gerenciador_telas = self.root.ids["screen_manager"]
+        gerenciador_telas.current = id_tela
 
 
 MainApp().run()
