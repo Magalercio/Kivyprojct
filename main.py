7e079b427c8f06c31a2c413b1c71ac24731641df
magalercio <magalercio.gui@gmail.com>
2022-09-20 20:50:37 -0300

    lista de fotos perfil
    Criado a funcao que carrega as infos do usuario logado.
    criado funcao de mudar foto do perfil
    conectando a alternancia de foto com o banco de dados.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from telas import *
 from botoes import *
 from bannervenda import BannerVenda
 import requests
+import os
+from functools import partial
 
 GUI = Builder.load_file("main.kv")  # tem que ficar depois de todas as classes de paginas, para que elas sejam criadas
 
@@ -18,6 +20,19 @@ class MainApp(App):
         return GUI
 
     def on_start(self):
+
+        # carregar as fotos de perfil
+        arquivos = os.listdir("icones/fotos_perfil")
+        pagina_foto_perfil = self.root.ids["fotoperfilpage"]
+        lista_fotos = pagina_foto_perfil.ids["lista_fotos_perfil"]
+        for foto in arquivos:
+            imagem = ImageButton(source=f"icones/fotos_perfil/{foto}", on_release=partial(self.mudar_foto_perfil, foto))
+            lista_fotos.add_widget(imagem)
+        # carrega as infos do usuários
+        self.carregar_infos_usuario()
+
+    def carregar_infos_usuario(self):
+
         # pegar informações do usuario
         requisicao = requests.get(f"https://aplicativovendaskiv-default-rtdb.firebaseio.com/{self.id_usuario}.json")
         requisicao_dic = requisicao.json()
@@ -30,13 +45,14 @@ class MainApp(App):
         # preencher listas de vendas
         try:
             vendas = requisicao_dic['vendas'][1:]
+            pagina_homepage = self.root.ids["homepage"]
+            lista_vendas = pagina_homepage.ids["lista_vendas"]
             for venda in vendas:
                 banner = BannerVenda(cliente=venda['cliente'], foto_cliente=venda['foto_cliente'],
                                      produto=venda['produto'], foto_produto=venda['foto_produto'],
                                      data=venda['data'], preco=venda['preco'], unidade=venda['unidade'],
                                      quantidade=venda['quantidade'])
-                pagina_homepage = self.root.ids["homepage"]
-                lista_vendas = pagina_homepage.ids["lista_vendas"]
+
                 lista_vendas.add_widget(banner)
         except:
             pass
@@ -45,5 +61,16 @@ class MainApp(App):
         gerenciador_telas = self.root.ids["screen_manager"]
         gerenciador_telas.current = id_tela
 
+    def mudar_foto_perfil(self, foto, *args):
+        print(f"{foto}")
+        foto_perfil = self.root.ids["foto_perfil"]
+        foto_perfil.source = f"icones/fotos_perfil/{foto}"
+
+        info = f'{{"avatar": "{foto}"}}'  # Chaves esta  dupla para o python entender que é chaves texto,
+        # não um valor formatado.
+
+        requisicao = requests.patch(f"https://aplicativovendaskiv-default-rtdb.firebaseio.com/{self.id_usuario}.json",
+                                    data=info)
+        self.mudar_tela("ajustespage")
 
 MainApp().run()
